c674630c1e134728e6fe935aaddf4d10cb503b45
Igor Ramon <ramon.riotinto@gmail.com>
2024-03-28 22:05:21 -0300

    feature: formatando datetimefild para padrao br

--- aluno/serializers.py
+++ aluno/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from .models import Alunos
 from professor.serializers import ProfessorSerializer
+from django.utils import timezone
 
 
 class AlunoSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     professor = ProfessorSerializer(read_only=True)
+    created_at = serializers.DateTimeField(format="%d/%m/%Y %H:%M:%S", read_only=True)
 
     class Meta:
         model = Alunos
@@ -20,8 +22,14 @@ class AlunoSerializer(serializers.ModelSerializer):
         read_only_fields = ["id", "created_at"]
         depth = 0
 
-    # Remover o campo professor da resposta da requisição
-    def to_representation(self, instance):
+    def format_date_america(self, instance):
+        data = super().to_representation(instance)
+        data["datetime_field"] = instance.datetime_field.astimezone(
+            timezone.get_current_timezone()
+        ).strftime("%d/%m/%Y %H:%M:%S")
+        return data
+
+    def remove_teacher_field(self, instance):
         data = super().to_representation(instance)
         data.pop("professor", None)
         return data
