f4fdf8262fd6b58ccb7cc3744346625088b15a87
Igor Ramon <ramon.riotinto@gmail.com>
2024-04-23 15:45:40 -0300

    add runtime, config procfile v2, gunicorn

--- ata_online/settings.py
+++ ata_online/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -92,7 +92,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": "LOCALHOST",
-        "POST": 5432,
+        "PORT": 5432,
     }
 }
 
