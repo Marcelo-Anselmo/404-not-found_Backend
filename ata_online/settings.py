51a963249a4d769b4f53581e11edcb8b2f27f83e
Igor Ramon <ramon.riotinto@gmail.com>
2024-04-23 20:55:18 -0300

    Config Render 3.0

--- ata_online/settings.py
+++ ata_online/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "404"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -133,9 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/mysite"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/ata_oline"
 
 if DATABASE_URL:
     db_from_env = dj_database_url.config(
